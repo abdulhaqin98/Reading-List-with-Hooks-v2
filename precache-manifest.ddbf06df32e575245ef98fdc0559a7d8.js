eb0554082d723923a84acd46ec085e07b004e734
Apple <apple@Apples-MacBook-Pro.local>
2020-05-01 16:10:34 +0530

    Updates

---
+++ precache-manifest.ddbf06df32e575245ef98fdc0559a7d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a1c1cbf9b0e8c458c6f191ea4aaa88",
+    "url": "/Reading-List-with-Hooks-v2/index.html"
+  },
+  {
+    "revision": "318dbc370b811e303137",
+    "url": "/Reading-List-with-Hooks-v2/static/css/main.847a716b.chunk.css"
+  },
+  {
+    "revision": "17c13d08c27484f665b5",
+    "url": "/Reading-List-with-Hooks-v2/static/js/2.df9c2391.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Reading-List-with-Hooks-v2/static/js/2.df9c2391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "318dbc370b811e303137",
+    "url": "/Reading-List-with-Hooks-v2/static/js/main.40af99eb.chunk.js"
+  },
+  {
+    "revision": "a15bac071d537cf591cd",
+    "url": "/Reading-List-with-Hooks-v2/static/js/runtime-main.524d0271.js"
+  }
+]);
